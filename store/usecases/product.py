11ea943b5d42391e0f5841e9ed116f20dfc99073
Kelrimy <143233886+kelrimy@users.noreply.github.com>
2024-07-03 12:35:34 -0300

    Update product.py

--- store/usecases/product.py
+++ store/usecases/product.py
@@ -2,11 +2,11 @@ from typing import List
 from uuid import UUID
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 import pymongo
+from datetime import datetime
 from store.db.mongo import db_client
 from store.models.product import ProductModel
 from store.schemas.product import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
-from store.core.exceptions import NotFoundException
-
+from store.core.exceptions import NotFoundException, InsertException
 
 class ProductUsecase:
     def __init__(self) -> None:
@@ -16,7 +16,10 @@ class ProductUsecase:
 
     async def create(self, body: ProductIn) -> ProductOut:
         product_model = ProductModel(**body.model_dump())
-        await self.collection.insert_one(product_model.model_dump())
+        try:
+            await self.collection.insert_one(product_model.model_dump())
+        except Exception as e:
+            raise InsertException(message="Failed to insert product") from e
 
         return ProductOut(**product_model.model_dump())
 
@@ -32,11 +35,15 @@ class ProductUsecase:
         return [ProductOut(**item) async for item in self.collection.find()]
 
     async def update(self, id: UUID, body: ProductUpdate) -> ProductUpdateOut:
+        body.updated_at = datetime.utcnow()
         result = await self.collection.find_one_and_update(
             filter={"id": id},
             update={"$set": body.model_dump(exclude_none=True)},
             return_document=pymongo.ReturnDocument.AFTER,
         )
+
+        if not result:
+            raise NotFoundException(message=f"Product not found with filter: {id}")
 
         return ProductUpdateOut(**result)
 
@@ -48,6 +55,5 @@ class ProductUsecase:
         result = await self.collection.delete_one({"id": id})
 
         return True if result.deleted_count > 0 else False
-
 
 product_usecase = ProductUsecase()
