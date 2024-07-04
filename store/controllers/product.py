60ac7ff5045a8e951e6f8f310dc54fae1a0a6247
Ludimar da Silva Padilha <59611203+LudimarPadilha@users.noreply.github.com>
2024-07-04 13:52:09 -0300

    Update product.py

--- store/controllers/product.py
+++ store/controllers/product.py
@@ -1,20 +1,19 @@
-from typing import List
 from fastapi import APIRouter, Body, Depends, HTTPException, Path, status
 from pydantic import UUID4
-from store.core.exceptions import NotFoundException
-
+from store.core.exceptions import NotFoundException, InsertException
 from store.schemas.product import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
 from store.usecases.product import ProductUsecase
 
 router = APIRouter(tags=["products"])
 
-
 @router.post(path="/", status_code=status.HTTP_201_CREATED)
 async def post(
     body: ProductIn = Body(...), usecase: ProductUsecase = Depends()
 ) -> ProductOut:
-    return await usecase.create(body=body)
-
+    try:
+        return await usecase.create(body=body)
+    except InsertException as exc:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=exc.message)
 
 @router.get(path="/{id}", status_code=status.HTTP_200_OK)
 async def get(
@@ -25,11 +24,9 @@ async def get(
     except NotFoundException as exc:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=exc.message)
 
-
 @router.get(path="/", status_code=status.HTTP_200_OK)
 async def query(usecase: ProductUsecase = Depends()) -> List[ProductOut]:
     return await usecase.query()
-
 
 @router.patch(path="/{id}", status_code=status.HTTP_200_OK)
 async def patch(
@@ -37,8 +34,10 @@ async def patch(
     body: ProductUpdate = Body(...),
     usecase: ProductUsecase = Depends(),
 ) -> ProductUpdateOut:
-    return await usecase.update(id=id, body=body)
-
+    try:
+        return await usecase.update(id=id, body=body)
+    except NotFoundException as exc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=exc.message)
 
 @router.delete(path="/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete(
