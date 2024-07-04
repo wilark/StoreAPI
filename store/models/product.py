5e58d766237f0a833989d654a1f471a50479ff89
Ludimar da Silva Padilha <59611203+LudimarPadilha@users.noreply.github.com>
2024-07-04 13:54:23 -0300

    Update product.py

--- store/models/product.py
+++ store/models/product.py
@@ -1,6 +1,27 @@
-from store.models.base import CreateBaseModel
-from store.schemas.product import ProductIn
+from pydantic import BaseModel, Field
+from typing import Optional
+from uuid import UUID, uuid4
+from datetime import datetime
+from decimal import Decimal
+from bson import Decimal128
+from pydantic import AfterValidator
 
+class ProductModel(BaseModel):
+    id: UUID = Field(default_factory=uuid4, alias="_id")
+    name: str
+    quantity: int
+    price: Decimal
+    status: bool
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
 
-class ProductModel(ProductIn, CreateBaseModel):
-    ...
+    class Config:
+        json_encoders = {
+            Decimal128: lambda v: Decimal128(str(v)),
+        }
+
+    @AfterValidator
+    def validate_decimal128(cls, v):
+        if isinstance(v, Decimal128):
+            return Decimal(v.to_decimal())
+        return v
